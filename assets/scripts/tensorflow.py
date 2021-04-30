33862b656119c0293f30727b23d974cd823ce443
lazyduo <dadanuna@gmail.com>
2021-05-01 00:26:19 +0900

    Nanum font adjust

--- assets/scripts/tensorflow.py
+++ assets/scripts/tensorflow.py
@@ -1,1 +1,7 @@
 import tensorflow as tf
+
+
+mnist = tf.keras.datasets.mnist # dataset load
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
